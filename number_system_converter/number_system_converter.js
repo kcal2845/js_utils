17e52f0aaf394b33595d37e9b85f9c40c2541fac
Minseok Kim <31371900+kcal2845@users.noreply.github.com>
2018-12-18 10:24:52 +0900

    Update number_system_converter.js

--- number_system_converter/number_system_converter.js
+++ number_system_converter/number_system_converter.js
@@ -1,77 +1,37 @@
-var frame_width = parseInt(document.getElementById('frame_width').value);
-var frame_height = parseInt(document.getElementById('frame_height').value);
-var frame_number = parseInt(document.getElementById('frame_number').value);
-var fps = parseInt(document.getElementById('fps').value);
-var frame = 0;
-var looping = false;
-var spritesheet = new Image();
-spritesheet.src= './test.png'
+input_binary = document.getElementById('input_binary');
+input_octal = document.getElementById('input_octal');
+input_decimal = document.getElementById('input_decimal');
+input_hexadecimal = document.getElementById('input_hexadecimal');
 
-document.getElementById('frame_width').oninput = function(){
-  frame_width = parseInt(this.value);
-}
-document.getElementById('frame_height').oninput = function(){
-  frame_height = parseInt(this.value);
-}
-document.getElementById('frame_number').oninput = function(){
-  frame = 0;
-  frame_number = parseInt(this.value);
-}
-document.getElementById('fps').oninput = function(){
-  fps = parseInt(this.value);
-}
-document.getElementById('animationbutton').onclick = function(){
-  if(looping){
-    this.value = 'animation start'
-  }else{
-    this.value = 'animation stop'
-  }
-  looping = !looping;
-}
-
-var file = document.querySelector('#getfile');
-file.onchange = function () {
-  var fileList = file.files ;
-
-  var reader = new FileReader();
-  reader.readAsDataURL(fileList [0]);
-
-  reader.onload = function  () {
-      spritesheet.src = reader.result
-  };
-};
-
-var cvs_ani = document.getElementById('canvas_animation');
-var ctx_ani = cvs_ani.getContext('2d');
-var cvs_spr = document.getElementById('canvas_spritesheet');
-var ctx_spr = cvs_spr.getContext('2d');
-
-spritesheet.onload= function(){
-  update();
-}
-
-function interval(){
-  setTimeout(update,1/fps*1000);
-}
-
-function update(){
-    ctx_spr.clearRect(0,0,cvs_spr.width,cvs_spr.height);
-    if(spritesheet.width>spritesheet.height){
-      ctx_spr.drawImage(spritesheet,0,0,spritesheet.width,spritesheet.height,0,0,
-        cvs_spr.width,cvs_spr.height*spritesheet.height/spritesheet.width);
-    }else{
-      ctx_spr.drawImage(spritesheet,0,0,spritesheet.width,spritesheet.height,0,0,
-        cvs_spr.width*spritesheet.width/spritesheet.height,cvs_spr.height);
+input_binary.oninput = function(){
+  if(this.value != ''){
+    if(this.value[this.value.length-1] != 0 && this.value[this.value.length-1] != 1){
+      this.value = this.value.slice(0,-1);
     }
-  if(looping){
-    ctx_ani.clearRect(0,0,cvs_ani.width,cvs_ani.height);
-    var cols = Math.floor(spritesheet.width/frame_width);
-    ctx_ani.drawImage(spritesheet,
-      frame % cols * frame_width, Math.floor(frame / cols) * frame_height,
-      frame_width, frame_height, 0, 0, frame_width, frame_height);
-    frame = (frame+1)%frame_number;
-    currentframetext.value = 'current frame : '+frame;
+    input_decimal.value = parseInt(this.value,2);
+    input_octal.value = parseInt(this.value,2).toString(8);
+    input_hexadecimal.value = parseInt(this.value,2).toString(16);
   }
-  sizetext.value = 'width : ' + spritesheet.width + ' height : ' + spritesheet.height;
-  interval();
+}
+input_octal.oninput = function(){
+  if(this.value != ''){
+    input_decimal.value = parseInt(this.value,8);
+    input_binary.value = parseInt(this.value,8).toString(2);
+    input_hexadecimal.value = parseInt(this.value,8).toString(16);
+  }
+}
+input_decimal.oninput = function(){
+  if(this.value != ''){
+    input_octal.value = parseInt(this.value,10).toString(8);
+    input_binary.value = parseInt(this.value,10).toString(2);
+    input_hexadecimal.value = parseInt(this.value,10).toString(16);
+  }
+}
+
+input_hexadecimal.oninput = function(){
+  if(this.value != ''){
+    input_decimal.value = parseInt(this.value,16);
+    input_binary.value = parseInt(this.value,16).toString(2);
+    input_octal.value = parseInt(this.value,16).toString(8);
+  }
 }
